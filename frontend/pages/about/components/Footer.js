6500052e4376a19ea5322bac7888764dacc9a9f2
Binhan <167419245+TomOutfit@users.noreply.github.com>
2025-05-30 19:35:28 +0700

    Modify AboutPage Footer Title

--- frontend/pages/about/components/Footer.js
+++ frontend/pages/about/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-black text-white text-center text-sm p-4 mt-10">
       <div className="border-t border-gray-700 pt-4">
-        <p>© 2024 Neth BookPoint</p>
+        <p>© 2025 Coder-BookStore</p>
         <p>Visit our branches in Galle, Kurunegala, Kandy, and Colombo</p>
         <div className="mt-2 flex justify-center gap-3">
           <a href="#" className="hover:text-yellow-400">Fb</a>
