166951616edb08b05a48d3236a67362852b88f83
Binhan <167419245+TomOutfit@users.noreply.github.com>
2025-05-30 17:54:32 +0700

    LoginPage

---
+++ frontend/pages/login/page.js
@@ -0,0 +1,11 @@
+import LoginForm from "../components/LoginForm";
+import LoginIllustration from "../components/LoginIllustration";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex">
+      <LoginIllustration />
+      <LoginForm />
+    </div>
+  );
+}
