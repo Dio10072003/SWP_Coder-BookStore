5dd2e21b8f9bf2a4d69ac08a5c6dcef691e02b93
dinhngocthien <dinhngocthien2004@gmail.com>
2025-06-02 13:10:42 +0700

    sửa

--- book-store-backend/src/models/book.ts
+++ book-store-backend/src/models/book.ts
@@ -1,7 +1,10 @@
 export interface Book {
-    id: string;
-    title: string;
-    author: string;
-    genre: string;
-    price: number;
+  id: string;
+  title: string;
+  author: string;
+  genre: string;
+  price: number;
+
+  year: number;
+  description?: string;
 }
