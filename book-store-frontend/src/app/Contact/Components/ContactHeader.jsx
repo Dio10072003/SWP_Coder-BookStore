d01561c65ed74364759c72a3d034a51dc2fc789c
phnbao2004 <phnbao2004@gmail.com>
2025-06-02 14:06:12 +0700

    contact page

---
+++ book-store-frontend/src/app/Contact/Components/ContactHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ContactHeader = () => {
+  return (
+    <div className="text-center mb-6">
+      <h1 className="text-3xl font-bold text-yellow-400">
+        Liên Hệ Với Chúng Tôi
+      </h1>
+      <p className="text-gray-400 mt-2">
+        Hỗ trợ và thông tin từ Coder-BookStore!
+      </p>
+    </div>
+  );
+};
+
+export default ContactHeader;
