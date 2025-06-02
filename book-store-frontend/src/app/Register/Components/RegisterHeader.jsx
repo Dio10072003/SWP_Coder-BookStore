611abdb4abd54f4da63c04ba4b1cd17514b33e58
dinhngocthien <dinhngocthien2004@gmail.com>
2025-06-02 13:48:25 +0700

    Register Pages

---
+++ book-store-frontend/src/app/Register/Components/RegisterHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const RegisterHeader = () => {
+  return (
+    <div className="text-center mb-6">
+      <h1 className="text-3xl font-bold text-yellow-400">
+        Tham Gia Coder-BookStore
+      </h1>
+      <p className="text-gray-400 mt-2">
+        Tạo tài khoản để khám phá kho sách lập trình!
+      </p>
+    </div>
+  );
+};
+
+export default RegisterHeader;
