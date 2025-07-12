15bf518a96c4c9001b71565e9fda344597ea5f75
TomOutfit <167419245+TomOutfit@users.noreply.github.com>
2025-07-12 16:36:22 +0700

    Add "use client" directive to BestPicksSection component

--- Coder_BookStore-Web/src/app/HomePage/Components/BestPicksSection.jsx
+++ Coder_BookStore-Web/src/app/HomePage/Components/BestPicksSection.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { addToCartLocal } from '../../utils/cartUtils';
 import { bookService } from '../../services/bookService';
