610c2eb29fc924362a79ffedd067325243293a70
phnbao2004 <phnbao2004@gmail.com>
2025-07-25 22:25:25 +0700

    Check next.config.js

--- Coder_BookStore-Web/next.config.js
+++ Coder_BookStore-Web/next.config.js
@@ -3,10 +3,9 @@ const nextConfig = {
     domains: [
       'api.dicebear.com',
       'images.unsplash.com',
-      // thêm các domain khác nếu cần
     ],
   },
-  // ...các cấu hình khác
+
 };
 
 module.exports = nextConfig; 
